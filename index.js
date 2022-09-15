648cfbcad590126d208e69be6305eef2f13c2cc5
Douglas Sattler <sattler.8@wright.edu>
2022-09-15 13:59:34 -0400

    maybe

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-var login = false;
-var who = "";
-
 function hash(str) {
     const utf8 = new TextEncoder().encode(str);
     return crypto.subtle.digest('SHA-256', utf8).then((hashBuffer) => {
@@ -26,25 +23,23 @@ async function check() {
         tessagreet.style.display = "block";
         document.getElementById("form").style.display = "none";
         wrong.style.display = "none";
-        login = true;
-        who = "Tessa";
+        sessionStorage.setItem("login", true);
+        sessionStorage.setItem("who", "Tessa");
     } else if ((username == "6132853eb8e76755d5646440abd7a00776a1facfa68e08fee859e454582c0b08") && (password == "b600cb9e7ed4f42e59d8a47b06288e1eef82e1680a54be9c9f4551ffa1ea9d88")) {
         megreet.style.display = "block";
         document.getElementById("form").style.display = "none";
         wrong.style.display = "none";
-        login = true;
-        who = "Doug";
+        sessionStorage.setItem("login", true);
+        sessionStorage.setItem("who", "Douglas");
     } else {
         wrong.style.display = "block";
-        login = false;
-        who = "";
     }
 }
 
 function checklogin() {
-    if (login == true) {
+    if (sessionStorage.getItem("login") == true) {
         document.getElementById("login").style.display = "none";
         document.getElementById("name").style.display = "block";
-        document.getElementById("name").innerHTML = who;
+        document.getElementById("name").innerHTML = sessionStorage.getItem("who");
     }
 }
