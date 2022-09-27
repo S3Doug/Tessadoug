5c270e080dd0265b583b9a48c304f3b93b665b7c
Douglas Sattler <sattler.8@wright.edu>
2022-09-27 16:10:41 -0400

    maybe

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+/* a function to hash an input */
 function hash(str) {
     const utf8 = new TextEncoder().encode(str);
     return crypto.subtle.digest('SHA-256', utf8).then((hashBuffer) => {
@@ -7,6 +9,7 @@ function hash(str) {
     });
 }
 
+/* a function to check if an inputted username and password are equivalent to the correct options */
 async function check() {
     let uname = document.getElementById("uname").value;
     let passw = document.getElementById("passw").value;
@@ -23,23 +26,25 @@ async function check() {
         tessagreet.style.display = "block";
         document.getElementById("form").style.display = "none";
         wrong.style.display = "none";
-        localStorage.setItem("login", true);
-        localStorage.setItem("who", "Tessa");
+        sessionStorage.setItem("login", true);
+        sessionStorage.setItem("who", "Tessa");
     } else if ((username == "6132853eb8e76755d5646440abd7a00776a1facfa68e08fee859e454582c0b08") && (password == "b600cb9e7ed4f42e59d8a47b06288e1eef82e1680a54be9c9f4551ffa1ea9d88")) {
         megreet.style.display = "block";
         document.getElementById("form").style.display = "none";
         wrong.style.display = "none";
-        localStorage.setItem("login", true);
-        localStorage.setItem("who", "Douglas");
+        sessionStorage.setItem("login", true);
+        sessionStorage.setItem("who", "Douglas");
     } else {
         wrong.style.display = "block";
     }
 }
 
-/*function checklogin() {
-    if (localStorage.getItem("login") == true) {
+function checklogin() {
+    var loggedin = sessionStorage.getItem("login");
+    if (loggedin) {
+        var who = sessionStorage.getItem("who");
         document.getElementById("login").style.display = "none";
         document.getElementById("name").style.display = "block";
-        document.getElementById("name").innerHTML = localStorage.getItem("who");
+        document.getElementById("name").innerHTML = who;
     }
-}*/
+}
