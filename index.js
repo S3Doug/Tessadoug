649c46a2b59beeb6cd72ae26b5e4506c92e349ab
Douglas Sattler <sattler.8@wright.edu>
2023-01-26 15:58:00 -0500

    Hi

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ async function check() {
         wrong.style.display = "none";
         sessionStorage.setItem("login", "true");
         sessionStorage.setItem("who", "Tessa");
+        window.location.href = "https://www.tessadoug.com/"
     } else if ((username == "6132853eb8e76755d5646440abd7a00776a1facfa68e08fee859e454582c0b08") && (password == "b600cb9e7ed4f42e59d8a47b06288e1eef82e1680a54be9c9f4551ffa1ea9d88")) {
         document.getElementById("form").style.display = "none";
         wrong.style.display = "none";
