050182565b60203992a5b981d57af21cf53a4dcc
Douglas Sattler <sattler.8@wright.edu>
2023-01-26 15:44:44 -0500

    Maybe

--- index.js
+++ index.js
@@ -14,8 +14,6 @@ async function check() {
     let uname = document.getElementById("uname").value;
     let passw = document.getElementById("passw").value;
     let wrong = document.getElementById("wrong");
-    let megreet = document.getElementById("megreet");
-    let tessagreet = document.getElementById("tessagreet");
     var username;
     var password;
     await hash(uname).then((hex) => username = hex);
@@ -23,17 +21,16 @@ async function check() {
     document.getElementById("uname").value = "";
     document.getElementById("passw").value = "";
     if ((username == "0fe9877170f58a0b7a7969545172bfe7eb9c4420b8b0a1f33b4cd2c41ead163e") && (password == "b600cb9e7ed4f42e59d8a47b06288e1eef82e1680a54be9c9f4551ffa1ea9d88")) {
-        tessagreet.style.display = "block";
         document.getElementById("form").style.display = "none";
         wrong.style.display = "none";
         sessionStorage.setItem("login", "true");
         sessionStorage.setItem("who", "Tessa");
     } else if ((username == "6132853eb8e76755d5646440abd7a00776a1facfa68e08fee859e454582c0b08") && (password == "b600cb9e7ed4f42e59d8a47b06288e1eef82e1680a54be9c9f4551ffa1ea9d88")) {
-        megreet.style.display = "block";
         document.getElementById("form").style.display = "none";
         wrong.style.display = "none";
         sessionStorage.setItem("login", "true");
         sessionStorage.setItem("who", "Douglas");
+        window.location.href = "https://www.tessadoug.com/"
     } else {
         wrong.style.display = "block";
     }
